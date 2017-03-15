dd6f690eb26e312372b27a430e11728884ae9bf7
Justin Ales <justin.ales@gmail.com>
2017-03-15 08:16:50 +0000

    test

---
+++ psychMaster/testFormat.m
@@ -0,0 +1,11 @@
+function [ output_args ] = untitled14( input_args )
+%UNTITLED14 Summary of this function goes here
+%   Detailed explanation goes here
+
+% $id$
+TestString = '$Format:%H$'
+
+ %  $Id$
+
+end
+
